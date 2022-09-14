0ce0858e02ab4d938aa4644fecb61bf7b4ae08a3
Олег Архипов <grimglance@mail.ru>
2022-09-14 23:48:51 +0700

    Добавьте файлы проекта.

---
+++ wumpus/Level.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wumpus
+{
+    public class Level
+    {
+        
+        public RoomFilling[,] _room  = new RoomFilling [8, 8];
+        
+        public void RoomFillingStart(Level level)
+        {
+           
+             _room[0,0] = RoomFilling.Entrance;
+          
+            
+            Console.WriteLine(level._room[0,0]);
+        }
+
+        public void DrawLevel(Level level, int x)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write($" |{level._room[i, j]}| ");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+        }
+
+
+
+    }
+
+
+  
+
+}
+
---
+++ wumpus/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(Му First C# Арр
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+// Обработать любые входные аргументы,
+for (int i = 0; i < args.Length; i++)
+» »
+*********
+и);
+{
+    Console.WriteLine("Arg: {0}", args[i]);
+}
+Console.ReadLine();
+// Возвратить произвольный код ошибки,
+return 0;
